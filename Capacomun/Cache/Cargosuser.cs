e6f918ede0efcb312083c96a96f0a5cdcbf8383a
HiuberMorente <hiuberr@gmail.com>
2021-10-21 15:15:12 -0600

    final

---
+++ Capacomun/Cache/Cargosuser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capacomun.Cache
+{
+    public struct Cargosuser
+    {
+        public const int Administrador = 2;
+        public const int Empleado = 1;
+
+    }
+}
